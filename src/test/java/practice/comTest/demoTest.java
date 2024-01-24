0368f8014e63b95e831061f233deea5157af00c7
HP <HP@DESKTOP-HOED7JD>
2024-01-24 18:44:45 +0530

    demo test has updated

---
+++ src/test/java/practice/comTest/demoTest.java
@@ -0,0 +1,19 @@
+package practice.comTest;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	
+	@Test
+	public void demotest()
+	{
+		System.out.println("demo test is executed");
+	}
+	
+	@Test
+	public void hellowTest()
+	{
+		System.out.println("hello test has not yet started");
+	}
+
+}
