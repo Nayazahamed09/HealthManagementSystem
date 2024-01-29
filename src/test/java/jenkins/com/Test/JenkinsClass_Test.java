64d7ab391d1696825db26b5a2ec4395310852a3f
Nayazahamed@09 <nayazahamed.91307@gmail.com>
2024-01-29 13:49:29 +0530

    JenkinsFile

---
+++ src/test/java/jenkins/com/Test/JenkinsClass_Test.java
@@ -0,0 +1,30 @@
+package jenkins.com.Test;
+
+import org.testng.annotations.Test;
+
+public class JenkinsClass_Test {
+	
+	@Test
+	public void methodOne()
+	{
+		System.out.println("Jenkins is launching");
+	}
+	@Test
+	public void methodTwo()
+	{
+		System.out.println("Jenkins is ready");
+	}
+	@Test
+	public void methodThree()
+	{
+		System.out.println("Jenkins is executed");
+	}
+	@Test
+	public void methodFour()
+	{
+		System.out.println("Jenkins is closed");
+	}
+
+
+
+}
